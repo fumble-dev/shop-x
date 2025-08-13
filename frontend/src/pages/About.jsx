386e81230df28fe1c547f45cf3c292f4091e6f86
Udanth <udanth.work@gmail.com>
2025-08-13 18:41:08 +0530

    login and about pages done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div >
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'About'} text2={'Us'} />
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full max-w-[450px]' alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>
+            At our core, we believe fashion is more than just clothing — it’s self-expression. Our platform brings together the latest trends, timeless classics, and unique designs so you can create a style that’s truly your own.
+          </p>
+          <p>
+            From everyday essentials to statement pieces, we handpick collections that blend quality, comfort, and style. Whether you’re refreshing your wardrobe or hunting for the perfect outfit, we make shopping effortless and inspiring.
+          </p>
+          <b className='text-gray-800'>
+            Our Mission
+          </b>
+          <p>
+            Our mission is to make fashion accessible and exciting for everyone. We strive to empower confidence through style, offering an inclusive range of products that cater to diverse tastes, sizes, and lifestyles — all while ensuring a seamless online shopping experience.
+          </p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4 '>
+        <Title text1={'Why'} text2={'Choose Us'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance</b>
+          <p className='text-gray-600'>
+            Every item in our store is carefully curated and inspected to meet the highest standards of craftsmanship. From premium fabrics to precise stitching, we ensure you receive products that are made to last and look exceptional.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience</b>
+          <p className='text-gray-600'>
+            Enjoy a smooth, stress-free shopping experience from the comfort of your home. With intuitive navigation, secure payment options, and quick delivery, finding your next favorite outfit has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service</b>
+          <p className='text-gray-600'>
+            Our friendly support team is here to assist you every step of the way. Whether you have questions about sizing, need styling advice, or require help with your order, we’re just a message away.
+          </p>
+        </div>
+      </div>
+
+      <NewsLetterBox/>
+
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up');
+
+  const onSubmitHandler = async(e) =>{
+    e.preventDefault();
+  }
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-md mx-auto mt-16 gap-6 text-gray-800">
+      <div className="flex items-center gap-3 mb-4">
+        <p className="text-3xl ">{currentState}</p>
+        <hr className="border-0 w-10 h-[2px] bg-gray-800" />
+      </div>
+
+      {currentState === 'Sign Up' ? <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required /> : ""}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required />
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+        <p className='cursor-pointer'>Forgot your Password?</p>
+        {
+          currentState === 'Login' ?
+            <p onClick={() => setCurrentState('Sign Up')} className='cursor-pointer'>Create Account</p>
+            : <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+        }
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4 cursor-pointer'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
 
