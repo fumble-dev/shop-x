fd957103517357fe4b75c852934de642ee3f82de
Udanth <udanth.work@gmail.com>
2025-08-17 10:45:03 +0530

    order page admin and user done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -41,15 +41,53 @@ const placeOrderRazorpay = async (req, res) => {
 }
 
 const allOrders = async (req, res) => {
-
+    try {
+        const orders = await orderModel.find({});
+        return res.json({
+            success: true,
+            orders: orders
+        })
+    } catch (error) {
+        console.error(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 const userOrders = async (req, res) => {
-
+    try {
+        const { userId } = req.body;
+        const orders = await orderModel.find({ userId })
+        res.json({
+            success: true,
+            orders: orders
+        })
+    } catch (error) {
+        console.error(error)
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 const updateStatus = async (req, res) => {
-
+    try {
+        const { orderId, status } = req.body;
+        await orderModel.findByIdAndUpdate(orderId, { status });
+        res.json({
+            success: true,
+            message: 'Stauts Updated'
+        })
+    } catch (error) {
+        console.error(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 export { placeOrder, placeOrderRazorpay, allOrders, updateStatus, userOrders }
